ff322d590af7dd41a263bf4da6bb6165d791cd71
SYCKGit <oviyangandhi@gmail.com>
2024-08-22 05:20:04 +0530

    ResourceItem editor [WIP]

---
+++ app/admin/categories/[categoryId]/[topicId]/[resourceId]/component.tsx
@@ -0,0 +1,32 @@
+"use client";
+import Latex from "@/components/latex";
+import { useState } from "react";
+
+interface Resource {
+  id: string;
+  title: string;
+  description: string | null;
+  content: string;
+}
+
+interface Author {
+  id: string;
+  name: string | null;
+}
+
+export default function ResourceEditorComponent({ resource, authors } : { resource: Resource, authors: Author[] }){
+  const [content, setContent] = useState(resource.content);
+  return (
+    <div className="grid grid-cols-2 space-x-5 flex-grow">
+      <textarea defaultValue={content} onInput={e => setContent(e.currentTarget.value)} className="w-full h-full dark:bg-gray-900 font-mono resize-none p-3 outline-none" />
+      <div className="text-lg">
+        <div className="mb-4">
+          <h1 className="text-3xl font-bold">{resource.title}</h1>
+          <h2 className="text-gray-500 mt-1">Authors:&nbsp;{authors.filter(a => a.name).map(a => a.name).join(", ")}</h2>
+        </div>
+        <p className="italic mb-4">{resource.description}</p>
+        <Latex>{content}</Latex>
+      </div>
+    </div>
+  )
+}
---
+++ app/admin/categories/[categoryId]/[topicId]/[resourceId]/page.tsx
@@ -0,0 +1,43 @@
+import { Button } from "@/components/ui/button";
+import { prisma } from "@/lib/utils.server";
+import Link from "next/link";
+import ResourceEditorComponent from "./component";
+
+export default async function ResourceEditor({ params } : { params: { resourceId: string } }){
+  const resource = await prisma.resourceItem.findUnique({
+    where: {
+      id: params.resourceId
+    },
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      content: true
+    }
+  });
+  if (!resource){
+    <div className="flex flex-col items-center justify-center h-full flex-grow gap-y-3">
+      <h1 className="text-4xl font-extrabold">404</h1>
+      <p className="text-xl">Resource not found</p>
+      <Link href="/">
+        <Button className="text-lg">
+          Back to Home
+        </Button>
+      </Link>
+    </div>
+  }
+  const authors = await prisma.resourceItem.findUnique({
+    where: {
+      id: params.resourceId
+    },
+    include: {
+      authors: {
+        select: {
+          id: true,
+          name: true
+        }
+      }
+    }
+  });
+  return <ResourceEditorComponent resource={resource!} authors={authors!.authors} />
+}
