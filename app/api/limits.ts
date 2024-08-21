cc6a1e37474f8fa74f59d9d263171eef0049e8b7
SYCKGit <oviyangandhi@gmail.com>
2024-08-22 05:18:36 +0530

    API ratelimit utility

---
+++ app/api/limits.ts
@@ -0,0 +1,39 @@
+import { findUserBySessionId } from "@/lib/utils.server";
+import { NextResponse } from "next/server";
+
+export class RateLimiter {
+  private per: number;
+  private last: Record<string, Date>
+
+  constructor(per: number) {
+    this.per = per;
+    this.last = {};
+  }
+
+  public hit(key: string): boolean {
+    if (!this.last[key]) {
+      this.last[key] = new Date();
+      return true;
+    }
+
+    const diff = new Date().getTime() - this.last[key].getTime();
+    if (diff > this.per) {
+      this.last[key] = new Date();
+      return true;
+    }
+
+    return false;
+  }
+}
+
+export function ratelimit(per: number, func: Function) {
+  const limiter = new RateLimiter(per);
+  return async function (...args: any[]) {
+    const user = await findUserBySessionId();
+    if (!user)
+      return NextResponse.json({ error: "You are not logged in" }, { status: 401 });
+    if (!limiter.hit(user.id))
+      return NextResponse.json({ error: "Rate limit exceeded" }, { status: 429 });
+    return await func(...args);
+  }
+}
