f7a0b82f88d96cdaa962349e4192c3d3dc9b296b
SYCKGit <oviyangandhi@gmail.com>
2024-08-23 00:02:16 +0530

    Revert "Reworked latex and markdown"
    This reverts commit 8c6931a8a7f151964ba0c167d98c12548704c9c4.

--- lib/utils.server.ts
+++ lib/utils.server.ts
@@ -1,5 +1,6 @@
-import { PrismaClient, User } from '@prisma/client'
+import {PrismaClient, User} from '@prisma/client'
 import { cookies } from 'next/headers';
+import { spawn } from "child_process";
 
 const prismaClientSingleton = () => {
   return new PrismaClient()
@@ -16,13 +17,13 @@ if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
 
 let auths: Record<string, User | undefined> = {}
 export const findUserBySessionId = async () => {
-
+  
   const store = cookies();
   let sessionId = store.get("__ssid")?.value;
   if (!sessionId) {
     return undefined;
   }
-  if (sessionId in auths) {
+  if ( sessionId in auths) {
     return auths[sessionId];
   }
   auths[sessionId] = (await prisma.sessionId.findUnique({
@@ -35,23 +36,24 @@ export const findUserBySessionId = async () => {
   }))?.user;
   return auths[sessionId];
 }
-// export function latexToHTML(content: string){
-//   return new Promise<string>((resolve, reject) => {
-//     const pandoc = spawn("pandoc", ["-f", "latex", "-t", "html"]);
 
-//     let html = "";
-//     let error = "";
-//     pandoc.stdout.on("data", (data) => html += data.toString());
-//     pandoc.stderr.on("data", (data) => error += data.toString());
+export function latexToHTML(content: string){
+  return new Promise<string>((resolve, reject) => {
+    const pandoc = spawn("pandoc", ["-f", "latex", "-t", "html"]);
 
-//     pandoc.on("close", (code) => {
-//         if (code === 0)
-//           resolve(html);
-//         else
-//           reject(new Error(error));
-//     });
+    let html = "";
+    let error = "";
+    pandoc.stdout.on("data", (data) => html += data.toString());
+    pandoc.stderr.on("data", (data) => error += data.toString());
 
-//     pandoc.stdin.write(content);
-//     pandoc.stdin.end();
-//   });
-// };
+    pandoc.on("close", (code) => {
+        if (code === 0)
+          resolve(html);
+        else
+          reject(new Error(error));
+    });
+
+    pandoc.stdin.write(content);
+    pandoc.stdin.end();
+  });
+};
