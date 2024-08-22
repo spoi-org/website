894490931b61415f59f3b766a49c1b893e29d0db
Aarav Malani <aarav.malani@gmail.com>
2024-08-22 23:48:05 +0530

    Fixed resource editor

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,5 @@
 import Users from "@/components/ui/admin/user";
-import { Button } from "@/components/ui/button";
-import { type User } from "@prisma/client";
 import Link from "next/link";
-import { useState } from "react";
 
 export default function Admin() {
     return (
